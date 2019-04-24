be99ba867a3fdd12cf608e9f694575d79d9a6cc1
Oliver Ian Krakora <oliver.krakora@allaboutapps.at>
2019-04-24 15:55:24 +0200

    finished reactiveexample

--- ReactiveExample/ReactiveExample/API.swift
+++ ReactiveExample/ReactiveExample/API.swift
@@ -70,38 +70,9 @@ public struct API {
         public static func list() -> Resource<[BlogPost]> {
             return Resource(
                 method: .get,
-                path: "/posts")
-        }
-        
-        public static func detail(id: Int) -> Resource<BlogPost> {
-            return Resource(
-                method: .get,
-                path: "/posts/\(id)")
-        }
-        
-        public static func create(_ post: BlogPost) -> Resource<BlogPost> {
-            return Resource(
-                method: .post,
                 path: "/posts",
-                body: [
-                    "title": post.title,
-                    "author": post.author
-                ])
-        }
-        
-        public static func nestedTest() -> Resource<BlogPost> {
-            return Resource(
-                method: .get,
-                path: "/mocked",
-                rootKeys: ["super", "deep", "nesting"],
                 shouldStub: true,
-                stub: StubResponse(statusCode: 200, fileName: "nested-post.json", delay: 1))
+                stub: StubResponse(statusCode: 200, fileName: "posts.json", delay: 1))
         }
-    }
-
-    public static func getEmpty() -> Resource<Empty> {
-        return Resource(
-            method: .get,
-            path: "/posts")
     }
 }
