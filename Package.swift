20e0cea5b15c329261e3524197a18d84f5a4f9f5
Stefan Wieland <stefan.wieland@allaboutapps.at>
2019-06-14 09:34:11 +0200

    add SPM support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Fetch",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "Fetch", targets: ["Fetch"])
+    ],
+    targets: [
+        .target(name: "Fetch", path: "Fetch/Code")
+        .testTarget(name: "FetchTests", dependencies: ["Fetch"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-beta.6"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
