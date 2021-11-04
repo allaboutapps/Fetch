efc7d61041804dbdfb38aeb784d4b5175759c593
stefan draskovits <stefan.draskovits@allaboutapps.at>
2021-11-04 10:39:50 +0100

    add xcode 13.2 support, async sequences + testing

---
+++ FetchTests/Async Await/AsyncTests.swift
@@ -0,0 +1,83 @@
+import XCTest
+import Alamofire
+import Fetch
+
+#if swift(>=5.5.2)
+
+@available(macOS 12, iOS 13, tvOS 15, watchOS 8, *)
+class AsyncTests: XCTestCase {
+    
+    override func setUp() {
+        APIClient.shared.setup(with: Config(
+            baseURL: URL(string: "https://www.asdf.at")!,
+            shouldStub: true
+        ))
+    }
+    
+    func testSuccessfulStubbingOfDecodable() {
+        let expectation = self.expectation(description: "Fetch model")
+        let resource = Resource<ModelA>(
+            method: .get,
+            path: "/test",
+            stub: StubResponse(statusCode: 200, encodable: ModelA(a: "a"), delay: 0.1))
+        
+        Task {
+            do {
+                let result = try await resource.requestAsync()
+                XCTAssertEqual(result.model.a, "a")
+                expectation.fulfill()
+            } catch {
+                XCTFail("Request did not return value")
+            }
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testFailingRequest() {
+        let expectation = self.expectation(description: "Fetch model")
+        let resource = Resource<ModelA>(
+            method: .get,
+            path: "/test",
+            stub: StubResponse(statusCode: 400, encodable: ModelA(a: "a"), delay: 0.1))
+        
+        Task {
+            do {
+                try await resource.requestAsync()
+                XCTFail("Request should not succeed")
+            } catch {
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testRequestTokenCanCancelRequest() {
+        let expectation = self.expectation(description: "T")
+        let stub = StubResponse(statusCode: 200, encodable: ModelA(a: "a"), delay: 0.2)
+        let resource = Resource<ModelA>(
+            method: .get,
+            path: "/test",
+            stub: stub)
+        
+        let task = Task {
+            do {
+                _ = try await resource.requestAsync()
+                XCTFail("Request should be cancelled")
+            } catch is CancellationError {
+                print("cancelled")
+            } catch {
+                XCTFail("Request should be cancelled")
+            }
+        }
+        task.cancel()
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + stub.delay + 0.1) {
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+}
+
+#endif
