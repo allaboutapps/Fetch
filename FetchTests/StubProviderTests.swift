c2026ded51b64dffb442d9ba3351a63bdff7faaf
Stefan Wieland <stefan.wieland@allaboutapps.at>
2021-12-09 11:31:32 +0100

    Remove carthage from example targets and use SPM instead

--- FetchTests/StubProviderTests.swift
+++ FetchTests/StubProviderTests.swift
@@ -18,19 +18,35 @@ class StubProviderTests: XCTestCase {
     }
 
     func testSuccessfulStubbingOfDecodable() {
-        let expectation = self.expectation(description: "Fetch model")
+        let expectation1 = self.expectation(description: "Fetch model")
         
         let resource = Resource<ModelA>(method: .get, path: "/test")
         
-        let stub = StubResponse(statusCode: 200, encodable: ModelA(a: "a"), delay: 0.1)
+        let stubA = StubResponse(statusCode: 200, encodable: ModelA(a: "a"), delay: 0.1)
         
-        APIClient.shared.config.stubProvider.register(stub: stub, for: resource.stubKey)
+        APIClient.shared.config.stubProvider.register(stub: stubA, for: resource.stubKey)
         
         resource.request { (result) in
             switch result {
             case let .success(value):
                 XCTAssertEqual(value.model.a, "a")
-                expectation.fulfill()
+                expectation1.fulfill()
+            default:
+                XCTFail("Request did not return value")
+            }
+        }
+        
+        let expectation2 = self.expectation(description: "Fetch model")
+        
+        let stubB = StubResponse(statusCode: 200, encodable: ModelA(a: "b"), delay: 0.1)
+        
+        APIClient.shared.config.stubProvider.register(stub: stubB, for: resource.stubKey)
+        
+        resource.request { (result) in
+            switch result {
+            case let .success(value):
+                XCTAssertEqual(value.model.a, "b")
+                expectation2.fulfill()
             default:
                 XCTFail("Request did not return value")
             }
