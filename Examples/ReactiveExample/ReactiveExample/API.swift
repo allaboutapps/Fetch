23f95376a4b4ad825ab6517a52e24a1e8b86e75f
Stefan Wieland <wielandweb@gmail.com>
2022-01-10 11:29:39 +0100

    clean up in api.swift

--- Examples/ReactiveExample/ReactiveExample/API.swift
+++ Examples/ReactiveExample/ReactiveExample/API.swift
@@ -17,7 +17,7 @@ public struct API {
         static let baseURL = URL(string: "")
         
         public static func login(username: String, password: String) -> Resource<Credentials> {
-            return Resource(
+            Resource(
                 method: .post,
                 baseURL: baseURL,
                 path: "/api/v1/auth/login",
@@ -25,59 +25,31 @@ public struct API {
                     "username": username,
                     "password": password
                 ]))
-//            , shouldStub: true,
-//                stub: StubResponse(statusCode: 200, fileName: "authresponse.json", delay: 3))
         }
         
         public static func tokenRefresh(_ refreshToken: String) -> Resource<Credentials> {
-            return Resource(
+            Resource(
                 method: .post,
                 baseURL: baseURL,
                 path: "/api/v1/auth/refresh",
                 body: .encodable([
                     "refreshToken": refreshToken
                 ]))
-//            , shouldStub: true,
-//                stub: StubResponse(statusCode: 200, fileName: "authresponse.json", delay: 4))
         }
         
         public static func authorizedRequest() -> Resource<Data> {
-            let conditionalStub = ClosureStub { () -> Stub in
-                let unauthorizedStub = StubResponse(statusCode: 401, data: Data(), delay: 2)
-                let okStub = StubResponse(statusCode: 200, data: Data(), delay: 2)
-                return CredentialsController.shared.currentCredentials == nil ? unauthorizedStub : okStub
-            }
-            
-            return Resource(
-                path: "/auth/secret")
-//            ,
-//                shouldStub: true,
-//                stub: conditionalStub
-//            )
+            Resource(path: "/auth/secret")
         }
         
         public static func unauthorizedErrorRequest() -> Resource<Data> {
-            let failingStub = StubResponse(statusCode: 401, data: Data(), delay: 2)
-            let okStub = StubResponse(statusCode: 200, data: Data(), delay: 2)
-            
-            return Resource(
-                path: "/fail")
-//            ,
-//                shouldStub: true,
-//                stub: AlternatingStub(stubs: [failingStub, okStub])
-//            )
+            Resource(path: "/fail")
         }
     }
     
     public struct BlogPosts {
         
         public static func list() -> Resource<[BlogPost]> {
-            return Resource(
-                method: .get,
-                path: "/posts")
-//            ,
-//                shouldStub: true,
-//                stub: StubResponse(statusCode: 200, fileName: "posts.json", delay: 1))
+            Resource(method: .get, path: "/posts")
         }
     }
 }
