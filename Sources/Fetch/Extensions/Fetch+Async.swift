dea41e789239e4d47b1e37cfd730de5b260eb4fd
Mathias Poimer <poimerm1@gmail.com>
2023-01-13 16:28:34 +0100

    lowered min version for use of async-await

--- Sources/Fetch/Extensions/Fetch+Async.swift
+++ Sources/Fetch/Extensions/Fetch+Async.swift
@@ -10,7 +10,7 @@
 
 import Foundation
 
-@available(macOS 12, iOS 13, tvOS 15, watchOS 8, *)
+@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 public extension Resource {
     
     enum ForwardBehaviour {
